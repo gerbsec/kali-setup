1e6e3f06dc93b647b4311f2486fc090307a65ba2
gerbsec <67713732+gerbsec@users.noreply.github.com>
2023-02-26 17:05:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,10 +61,9 @@ espanso service register
 espanso start
 cp ~/DotFiles/dots/base.yml ~/.config/espanso/match/base.yml
 cp ~/DotFiles/dots/.tmux.conf ~/.tmux.conf
-
-
+echo "type exit after zsh intall"
+sleep 10
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-exit
 cp ~/DotFiles/dots/.zshrc ~/.zshrc
 
 
