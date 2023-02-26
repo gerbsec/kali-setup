75c3a02e9154d64907f6e7499670ec288496959f
gerbsec <gerbsec@proton.me>
2023-02-26 13:01:47 -0800

    all the stuffs

--- dotFiles.sh
+++ dotFiles.sh
@@ -2,68 +2,76 @@ echo "Script to set up VM on spawn"
 echo "No need to run with root, if it needs sudo it'll ask for it just be ready to enter the password"
 echo "These are my settings not yours so haha"
 sleep 1
-echo "firefox config? y/n"
-read firefox
-if [[ $firefox == "y" ]];then
-    echo "Setting up firefox search engines"
-    for i in $(find ~ | grep firefox | grep search.json); do cp firefoxConfig/search.json.mozlz4 $i 2>/dev/null;done
-    echo "Setting up firefox preferences"
-    for i in $(find ~ | grep firefox | grep prefs.js); do cp firefoxConfig/prefs.js $i 2>/dev/null;done
-    echo "Setting up firefox extensions"
-    for i in $(find ~ | grep firefox| grep default | grep -v cache);do
-        if [[ $i == *release ]];then
-            dir=$i
-        fi
-    done
-    mkdir -p $dir/extensions
-    mkdir -p extensions && cd extensions
-    wget https://addons.mozilla.org/firefox/downloads/file/4028976/ublock_origin-1.45.2.xpi
-    unzip ublock_origin-1.45.2.xpi
-    name=$(cat manifest.json | grep \"id\" | awk -F \" '{print $4}')
-    mv ublock_origin-1.45.2.xpi $dir/extensions/$name.xpi
-    rm -rf extensions
-    mkdir -p extensions && cd extensions
-    wget https://addons.mozilla.org/firefox/downloads/file/3616824/foxyproxy_standard-7.5.1.xpi
-    unzip foxyproxy_standard-7.5.1.xpi
-    name=$(cat manifest.json | grep \"id\" | awk -F \" '{print $4}')
-    mv foxyproxy_standard-7.5.1.xpi $dir/extensions/$name.xpi
-    rm -rf extensions
-    mkdir -p extensions && cd extensions
-    wget https://addons.mozilla.org/firefox/downloads/file/3755764/cookie_editor-1.10.1.xpi
-    unzip cookie_editor-1.10.1.xpi
-    name=$(cat manifest.json | grep \"id\" | awk -F \" '{print $4}')
-    mv cookie_editor-1.10.1.xpi $dir/extensions/$name.xpi
-    rm -rf extensions
-    mkdir -p extensions && cd extensions
-    wget https://addons.mozilla.org/firefox/downloads/file/3952467/user_agent_string_switcher-0.4.8.xpi
-    unzip user_agent_string_switcher-0.4.8.xpi
-    name=$(cat manifest.json | grep \"id\" | awk -F \" '{print $4}')
-    mv user_agent_string_switcher-0.4.8.xpi $dir/extensions/$name.xpi
-    rm -rf extensions
-elif [[ $firefox == "n" ]];then
-    echo "ok moving on"
-else
-    echo "please choose y/n"
-    exit 1
-fi
+for i in $(find ~ | grep firefox | grep search.json); do cp firefoxConfig/search.json.mozlz4 $i 2>/dev/null;done
+echo "Setting up firefox preferences"
+for i in $(find ~ | grep firefox | grep prefs.js); do cp firefoxConfig/prefs.js $i 2>/dev/null;done
 
 echo "dotfiles config? y/n"
-read dotfiles
-if [[ $dotfiles == "y" ]];then
-    sudo dpkg --add-architecture i386
-    sudo apt update && sudo apt install curl zsh tmux vim binutils libc6:i386 libncurses5:i386 libstdc++6:i386 openjdk-17-jdk -y
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    go=$(curl https://go.dev/dl/ -s 2>/dev/null | grep linux | grep amd64 | head -n 1 | awk -F \" '{print $4}')
-    wget https://go.dev$go
-    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $(echo $go | awk -F "/" '{print $3}')
-    rm -rf $(echo $go | awk -F "/" '{print $3}')
-    mkdir -p ~/go
-    cp ~/repo/DotFiles/dots/.zshrc ~/.zshrc
-    cp ~/repo/DotFiles/dots/.tmux.conf ~/.tmux.conf
-    bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
-elif [[ $dotfiles == "n" ]];then
-    echo "nay"
-else
-    echo "please choose y/n"
-    exit 1
-fi
+sudo dpkg --add-architecture i386
+sudo apt update && sudo apt install curl zsh tmux vim binutils dirsearch libc7:i386 libncurses5:i386 libstdc++6:i386 openjdk-17-jdk -y
+sudo apt install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
+sudo apt install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
+sudo apt install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
+sudo apt autoremove && sudo apt autoclean -y
+go=$(curl https://go.dev/dl/ -s 2>/dev/null | grep linux | grep amd64 | head -n 1 | awk -F \" '{print $4}')
+wget https://go.dev$go
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $(echo $go | awk -F "/" '{print $3}')
+rm -rf $(echo $go | awk -F "/" '{print $3}')
+mkdir -p ~/go
+bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
+cp ~/repo/DotFiles/dots/.tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.local/share/fonts/
+
+curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest  | grep -E "browser_download_url.*Iosevka.zip" | cut -d : -f 2,3 | tr -d \" |  wget -qi -
+curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest  | grep -E "browser_download_url.*RobotoMono.zip" | cut -d : -f 2,3 | tr -d \" |  wget -qi -
+
+unzip Iosevka.zip -d ~/.local/share/fonts/
+unzip RobotoMono.zip -d ~/.local/share/fonts/
+
+fc-cache -fv
+
+wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.0-rc4-1/alacritty_0.10.0-rc4-1_amd64_bullseye.deb
+curl -s https://api.github.com/repos/barnumbirr/alacritty-debian/releases/latest  | grep -E "browser_download_url.*bullseye.deb" | cut -d : -f 2,3 | tr -d \" |  wget -qi -
+sudo dpkg -i *bullseye.deb
+sudo apt install -f
+
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps && mkdir -p build && cd build && meson ..
+ninja
+sudo ninja install
+cd ../..
+
+pip3 install pywal
+
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/compton
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/alacritty
+cp .config/i3/config ~/.config/i3/config
+cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+cp .config/compton/compton.conf ~/.config/compton/compton.conf
+cp .config/rofi/config ~/.config/rofi/config
+cp .fehbg ~/.fehbg
+cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
+
+# Retina Display - DPI Fix
+echo 'Xft.dpi: 172' > ~/.Xresources
+echo '[ -r /home/kali/.config/kali-HiDPI/xsession-settings ] && . /home/kali/.config/kali-HiDPI/xsession-settings
+xrandr --dpi 172
+export XCURSOR_SIZE=48' > ~/.xsessionrc
+
+echo "Installing Espanso"
+curl -s https://api.github.com/repos/espanso/espanso/releases/latest  | grep -E "browser_download_url.*Espanso-X11.AppImage" | cut -d : -f 2,3 | tr -d \" | grep -v sha256 | wget -qi - -O espanso
+chmod +x espanso && sudo mv espanso /usr/bin/espanso 
+espanso service register
+espanso start
+wget 
+
+echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme. To have the wallpaper set on every boot edit ~.fehbg"
+echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
+echo "get burp pro :D"
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
